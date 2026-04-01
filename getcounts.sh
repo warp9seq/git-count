63619fb56bddaba74a910bca4bdd7a5bd9ffb02b
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2026-04-01 23:08:58 +1100

    update

--- getcounts.sh
+++ getcounts.sh
@@ -6,6 +6,10 @@ die() {
 	exit 1
 }
 
+
+python3 sumup.py minimod > minimod.txt
+python3 sumup.py openfish > openfish.txt
+
 git config --global user.email "action@github.com" || die "Failed to configure git email"
 git config --global user.name "GitHub Action" || die "Failed to configure git name"
 git add minimod/ openfish/ || die "adding failed"
@@ -13,10 +17,5 @@ if ! git diff --cached --quiet; then
     git commit -m "Update download counts" || die "commit failed"
 	git push || die "push failed"
 fi
-
-python3 sumup.py minimod > minimod.txt
-python3 sumup.py openfish > openfish.txt
-
-
 
 
