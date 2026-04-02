f541cef17cbe4e28908e2862279addbe379b7125
GitHub Action <action@github.com>
2026-04-02 19:45:27 +1100

    json

--- getcounts.sh
+++ getcounts.sh
@@ -10,11 +10,14 @@ LIST="minimod openfish kuva buttery-eel blue-crab f5c slow5lib slow5tools sigtk 
 
 for each in $LIST; do
 	python3 sumup.py ${each} > ${each}.txt
+	T=$(grep "Total clones" ${each}.txt | awk '{print $NF}')
+	U=$(grep "Total uniques" ${each}.txt | awk '{print $NF}')
+	echo '{"schemaVersion": 1, "label": "total clones", "message": "count: '${T}' | uniques: '${U}'" }' > ${each}.json
 done
 
 git config --global user.email "action@github.com" || die "Failed to configure git email"
 git config --global user.name "GitHub Action" || die "Failed to configure git name"
-git add *.txt ${LIST} || die "adding failed"
+git add *.txt *.json ${LIST} || die "adding failed"
 if ! git diff --cached --quiet; then
     git commit -m "Update download counts" || die "commit failed"
 	git push || die "push failed"
